d14d15e344a4ac4a536dc74af46f35eab3db07c5
kak Shell <73112436+kaklahur123@users.noreply.github.com>
2021-11-16 14:53:41 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    defaultPrefix: 'h!',
-    ownerID: '681553671364018196',
+    defaultPrefix: '',
+    ownerID: '',
     _limits: 'The following are defaults.',
     adminCanChangeLimits: true,
     limits: {
